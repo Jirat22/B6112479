50bda72d8dc86fa41afd3d6695749f5f1b1cde7a
Jirat22 <jiratz2208@gmail.com>
2020-12-30 13:34:54 +0700

    Create Motorcycle_cash Entity - close#3

---
+++ backend/ent/schema/motorcycle_cash.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Motorcycle_cash holds the schema definition for the Motorcycle_cash entity.
+type Motorcycle_cash struct {
+	ent.Schema
+}
+
+// Fields of the Motorcycle_cash.
+func (Motorcycle_cash) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Motorcycle_cash.
+func (Motorcycle_cash) Edges() []ent.Edge {
+	return nil
+}
